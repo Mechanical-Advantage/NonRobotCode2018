580c430a5b5009766de63d08901620cd948abf96
e-bonner <e-bonner@users.noreply.github.com>
2018-03-07 21:05:33 -0500

    Add support for using high bits to split bytes

--- Arduino/frcOILEDs/frcOILEDs.ino
+++ Arduino/frcOILEDs/frcOILEDs.ino
@@ -1,3 +1,6 @@
+#define SET_BITS 2
+#define DATA_MASK 0x3f // Should have data bits set, others 0, must match SET_BITS
+
 byte ledPins[] = {
   9, 8, 10, 7, 11, A0, A1, 12
 };       // an array of pin numbers to which LEDs are attached
@@ -5,12 +8,12 @@ byte ledPins[] = {
 void setup() {
   Serial.begin(9600);
   byte thisPin;
-  for (thisPin = 0; thisPin < 8; thisPin++) {
+  for (thisPin = 0; thisPin < sizeof(ledPins); thisPin++) {
     pinMode(ledPins[thisPin], OUTPUT);
     digitalWrite(ledPins[thisPin], HIGH);
   }
   delay(750);
-  for (thisPin = 0; thisPin < 8; thisPin++) {
+  for (thisPin = 0; thisPin < sizeof(ledPins); thisPin++) {
     digitalWrite(ledPins[thisPin], LOW);
   }
 }
@@ -18,25 +21,29 @@ void setup() {
 void loop() {
   byte serialByte;
   int available = Serial.available();
-  // this makes sure we get the last byte
-  for (int curByte = 0; curByte < available; curByte++) {
-    serialByte = Serial.read();
-  }
+  serialByte = Serial.read();
 
   if (available > 0) {
-    Serial.println();
-    for (byte thisPin = 0; thisPin < 8; thisPin++) {
-      if (serialByte >= 1<<(7-thisPin)) {
-        digitalWrite(ledPins[thisPin], HIGH);
-        /*Serial.print("Setting LED ");
-        Serial.print(String(ledPins[thisPin]));
-        Serial.println(" to HIGH");*/
-        serialByte-=1<<(7-thisPin);
-      } else {
-        digitalWrite(ledPins[thisPin], LOW);
-        /*Serial.print("Setting LED ");
-        Serial.print(String(ledPins[thisPin]));
-        Serial.println(" to LOW");*/
+    //Serial.println();
+    // Get the set index
+    byte setIndex = serialByte>>(8-SET_BITS);
+    // Clear set index from byte
+    serialByte &= DATA_MASK;
+    for (byte thisPin = 0; thisPin < 8-SET_BITS; thisPin++) {
+      byte ledIndex = thisPin+((8-SET_BITS)*setIndex);
+      if (ledIndex < sizeof(ledPins)) {
+        if (serialByte >= 1<<(7-SET_BITS-thisPin)) {
+          digitalWrite(ledPins[ledIndex], HIGH);
+          /*Serial.print("Setting LED ");
+          Serial.print(String(ledPins[ledIndex]));
+          Serial.println(" to HIGH");*/
+          serialByte-=1<<(7-SET_BITS-thisPin);
+        } else {
+          digitalWrite(ledPins[ledIndex], LOW);
+          /*Serial.print("Setting LED ");
+          Serial.print(String(ledPins[ledIndex]));
+          Serial.println(" to LOW");*/
+        }
       }
     }
   }
--- Python/frcOILEDs.py
+++ Python/frcOILEDs.py
@@ -4,8 +4,11 @@ from time import sleep
 from networktables import NetworkTables
 from serial import Serial
 
-NTSERVER = "10.63.28.2"
-SERIAL_PORT = "/dev/cu.usbmodem411"
+NTSERVER = "127.0.0.1"
+SERIAL_PORT = "/dev/cu.usbmodem241141"
+SET_BITS = 2
+
+last_value = []
 
 # serial port setup
 arduino = Serial(SERIAL_PORT, 9600)
@@ -13,9 +16,26 @@ arduino = Serial(SERIAL_PORT, 9600)
 sleep(2)
 
 def update_values(table, key, value, isNew):
-    binStr = ''.join(['1' if x else '0' for x in value])
-    arduino.write(bytes([int(binStr, base=2)]))
-    print(binStr)
+    global last_value
+    diff = [] # Will have true if the bit changed
+    serial_data = []
+    if len(last_value) < len(value):
+        last_value = [False]*len(value)
+    for old, new in zip(last_value, value):
+        diff.append(False if old == new else True)
+    byte_diff = [diff[i:i + 8-SET_BITS] for i in range(0, len(diff), 8-SET_BITS)]
+    send_bits = [value[i:i + 8-SET_BITS] for i in range(0, len(value), 8-SET_BITS)]
+    for index, item_diff, bits in zip(range(len(send_bits)), byte_diff, send_bits):
+        if True in item_diff:
+            # Ensure bits is of the proper length, must be list
+            bits = list(bits)
+            bits.extend([False]*((8-SET_BITS)-len(bits)))
+            byte_index = index<<8-SET_BITS
+            data_string = ''.join(['1' if x else '0' for x in bits])
+            serial_data.append(int(data_string, base=2)+byte_index)
+            print(index, data_string, sep=", ")
+    last_value = value
+    arduino.write(bytes(serial_data))
 
 # NetworkTables setup
 NetworkTables.initialize(server=NTSERVER)
